6a0d8e48a7cdb0cfb899fc785b4e7f734952d99a
Mike123729 <konst.nikolas@gmail.com>
2022-08-05 17:18:58 +0300

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const formData = localStorage.getItem(LOCAL_STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  : {};
+
+formRef.addEventListener('input', throttle(inputData, 500));
+formRef.addEventListener('submit', toSubmit);
+
+setFormData();
+
+function inputData({ target: { name, value } }) {
+  formData[name] = value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function setFormData() {
+  const emailRef = formRef.elements.email;
+  const messageRef = formRef.elements.message;
+
+  const localStorageData = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (localStorageData) {
+    const data = JSON.parse(localStorageData);
+    if (data.email) {
+      emailRef.value = data.email;
+    }
+    if (data.message) {
+      messageRef.value = data.message;
+    }
+  }
+}
+
+function toSubmit(e) {
+  e.preventDefault();
+
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+
+  if (email === '' || message === '') {
+    alert('Вы заполнили не все поля!');
+    return;
+  }
+  formRef.reset();
+  console.log({ email, message });
+}
